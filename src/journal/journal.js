68da53feed25462c410a23bba9c19adb991748d9
ymy931001 <ymy931001>
2019-09-30 14:24:04 +0800

    9.30

--- src/journal/journal.js
+++ src/journal/journal.js
@@ -21,7 +21,7 @@ const dateFormat = 'YYYY/MM/DD HH:mm:ss';
 
 
 var now = new Date();
-var date = new Date(now.getTime() - 2*24 * 3600 * 1000);
+var date = new Date(now.getTime() - 7*24 * 3600 * 1000);
 var year = date.getFullYear();
 var month = date.getMonth() + 1;
 var day = date.getDate();
